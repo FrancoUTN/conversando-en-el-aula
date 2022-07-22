dc371ff91646b95a44928dd1e2f5c22c6ec2d478
unknown <cataniafrancoar@gmail.com>
2022-07-22 20:00:09 -0300

    TextInput e Ionicon

--- src/screens/AulaScreen.js
+++ src/screens/AulaScreen.js
@@ -1,14 +1,17 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, TextInput } from 'react-native';
 import { collection, getDocs } from 'firebase/firestore';
 
 import { database } from '../util/fire';
 import GiantButton from '../components/ui/GiantButton';
 import { Colors } from '../constants/styles';
-import { TextInput } from 'react-native-web';
 import Input from '../components/Auth/Input';
+import { useState } from 'react';
+import IconButton from '../components/ui/IconButton';
 
 
 export default function AulaScreen({route}) {
+  const [textoMensaje, setTextoMensaje] = useState('');
+
   async function firebaseTestHandler() {
     const querySnapshot = await getDocs(collection(database, "usuarios"));
 
@@ -19,18 +22,28 @@ export default function AulaScreen({route}) {
 
   const color = route.params?.division === 'PPS-4A' ? Colors.pps4a : Colors.pps4b;
 
+  function onChangeTextHandler(texto) {
+    setTextoMensaje(texto);
+  }
+
   return (
     <View style={[styles.rootContainer, {backgroundColor: color}]}>
-      <View>
+      <View style={styles.listaContainer}>
 
       </View>
-      {/* <TextInput
-        style={styles.input}
-        // keyboardType={keyboardType}
-        // secureTextEntry={secure}
-        // onChangeText={onUpdateValue}
-        // value={value}
-      /> */}
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          value={textoMensaje}
+          onChangeText={onChangeTextHandler}
+        />
+        <IconButton
+          icon="send"
+          color={'white'}
+          size={20}
+          onPress={onChangeTextHandler}
+        />
+      </View>
     </View>
   );
 }
@@ -39,21 +52,27 @@ const styles = StyleSheet.create({
   rootContainer: {
     flex: 1
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 8,
+  listaContainer: {
+    flex: 7
   },
-  botonGigante: {
+  inputContainer: {
     flex: 1,
-    justifyContent: 'center',
-    // alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: 'gray',
+    // justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingVertical: 20
   },
   input: {
+    flex: 1,
+    marginRight: 10,
     paddingVertical: 8,
     paddingHorizontal: 6,
-    // backgroundColor: ,
+    backgroundColor: 'white',
     borderRadius: 4,
     fontSize: 16,
+    height: 40,
   },
 });
