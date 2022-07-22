e56ebfc3dc523d4dc8d7534a3846b242b52eed6a
unknown <cataniafrancoar@gmail.com>
2022-07-22 17:44:20 -0300

    util/fire.js

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,20 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
-import { initializeApp } from 'firebase/app';
-import { getFirestore, setDoc, doc, getDocs, collection } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAe_uNqlwLgWeOYoqWjc0k26qCoPSBMXbw",
-  authDomain: "react-native-79690.firebaseapp.com",
-  projectId: "react-native-79690",
-  storageBucket: "react-native-79690.appspot.com",
-  messagingSenderId: "509196424145",
-  appId: "1:509196424145:web:02fff0fcf2f6c444ab06f0"
-};
-
-initializeApp(firebaseConfig);
-
-const firestore = getFirestore();
-
+import { collection, getDocs } from 'firebase/firestore';
+import { firestore } from '../util/fire';
 
 function WelcomeScreen() {
   async function firebaseTestHandler() {
---
+++ src/util/fire.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAe_uNqlwLgWeOYoqWjc0k26qCoPSBMXbw",
+  authDomain: "react-native-79690.firebaseapp.com",
+  projectId: "react-native-79690",
+  storageBucket: "react-native-79690.appspot.com",
+  messagingSenderId: "509196424145",
+  appId: "1:509196424145:web:02fff0fcf2f6c444ab06f0"
+};
+
+initializeApp(firebaseConfig);
+
+export const firestore = getFirestore();
